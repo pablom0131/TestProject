c78c18063af1a6922eb8fcb7fea3ddf7bb662827
pablom0131 <78882962+pablom0131@users.noreply.github.com>
2021-02-14 22:00:46 -0800

    first commit

--- Eclipse Reinstallation Test/src/TestClassTwenty.java
+++ Eclipse Reinstallation Test/src/TestClassTwenty.java
@@ -3,7 +3,7 @@ public class TestClassTwenty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i hate myself a lot");
+		System.out.println("i hate myself a lot because im trying to figure this out");
 	}
 
 }
