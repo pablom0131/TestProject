524f84ad3784b955f114f546e916c91a866d135f
pablom0131 <78882962+pablom0131@users.noreply.github.com>
2021-02-14 22:15:47 -0800

    second commit

--- Eclipse Reinstallation Test/src/TestClassTwenty.java
+++ Eclipse Reinstallation Test/src/TestClassTwenty.java
@@ -3,7 +3,7 @@ public class TestClassTwenty {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hm hm does this work?");
+		System.out.println("hm hm does this work? OMG I THINK IT DOES WORK!");
 	}
 
 }
